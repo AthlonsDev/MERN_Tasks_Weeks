b36dda4cff6fa9a057ee9d53d4aa89169fd5117e
AthlonsDev <47662221+AthlonsDev@users.noreply.github.com>
2022-10-12 18:20:36 +0100

    MERN_Tasks

---
+++ index.js
@@ -0,0 +1,30 @@
+
+// function EmployeeInfo(name, Salary) {
+//     console.log("Welcome " + name + " Your monthly salary is " + Salary)
+// }
+
+// const empSkills = (skills) => {
+//     return ("Expert in " + skills)
+// }
+
+// console.log("this sis my first program");
+
+// var empName = "John"
+// var empSalary = 50000
+
+// EmployeeInfo(empName, empSalary)
+// console.log(empSkills("java"))
+
+// const os = require("os")
+// const util = require("util")
+
+// console.log(os.uptime()/3600)
+// console.log(util.inspect(os.cpus()))
+
+
+
+
+
+const rectangle = require("./rectangle")
+
+console.log(rectangle.perimeter(20, 10))
---
+++ rectangle.js
@@ -0,0 +1,12 @@
+
+
+exports.perimeter = (breadth, length) => {
+    return ("Perimeter of rectangle is " + 2*(length*breadth))
+}
+const area = (length, breadth) => {
+    return ("Area of rectangle is " + (lenght*breadth))
+}
+
+
+// exports.perimeter = getRectanlgPerimeter
+// exports.
